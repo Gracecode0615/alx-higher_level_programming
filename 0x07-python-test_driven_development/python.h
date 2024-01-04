4ebbc8aa5336e7727a1039256e3c6ae571e8990a
Grace0615 <graceebruwiyor@gmail.com>
2024-01-04 07:43:31 +0100

    update

---
+++ 0x07-python-test_driven_development/python.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX project
+ */
+
+def add_integer(a, b=98)
+def matrix_divided(matrix, div)
+def say_my_name(first_name, last_name="")
+def print_square(size)
+def text_indentation(text)
+def max_integer(list=[])
+def matrix_mul(m_a, m_b)
+void print_python_string(PyObject *p)
+
+#endif /* LISTS_H */
